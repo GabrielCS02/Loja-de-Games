1e1607716e1438a5f0dea824a37b92b192583a95
Vitor Matheus <vmatheushs@gmail.com>
2021-12-14 12:24:04 -0300

    Controller Usuário

---
+++ Loja_de_Games/src/main/java/br/com/horizongames/controller/UsuarioController.java
@@ -0,0 +1,22 @@
+package br.com.horizongames.controller;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.CadastrarUsuario(usuario));
+	}
+}
